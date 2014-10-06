a22119a3b69591414f1123e20bf927f27078daec
Vianney Dupoy de Guitard <lain@n4v1.com>
2014-10-06 15:15:05 +0200

    Class renamed to comply with coding convention.

--- moviesearch/src/main/java/fr/univtls2/web/moviesearch/services/indexation/ImportService.java
+++ moviesearch/src/main/java/fr/univtls2/web/moviesearch/services/indexation/ImportService.java
@@ -2,7 +2,7 @@ package fr.univtls2.web.moviesearch.services.indexation;
 
 import java.io.File;
 
-public interface importService {
+public interface ImportService {
 
 	void start(final File directory);
 }
