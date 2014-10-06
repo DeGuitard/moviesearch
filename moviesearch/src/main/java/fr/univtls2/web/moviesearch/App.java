eec5f36a50553c5f57bdc0b5f449f064200abb3d
Vianney Dupoy de Guitard <lain@n4v1.com>
2014-10-06 10:46:42 +0200

    Initial commit.

---
+++ moviesearch/src/main/java/fr/univtls2/web/moviesearch/App.java
@@ -0,0 +1,13 @@
+package fr.univtls2.web.moviesearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
