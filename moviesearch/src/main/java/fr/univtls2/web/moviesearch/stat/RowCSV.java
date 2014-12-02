ad007a1622795f98d05b3a55b2e61a125af820f4
Guillaume Ruscassie <guillaume.ruscassie@gmail.com>
2014-12-02 17:01:52 +0100

    replace the decimal separtor . => , and replace the separtor csv by ;

--- moviesearch/src/main/java/fr/univtls2/web/moviesearch/stat/RowCSV.java
+++ moviesearch/src/main/java/fr/univtls2/web/moviesearch/stat/RowCSV.java
@@ -4,7 +4,9 @@ public class RowCSV {
 
 	private String[] columns;
 
-	private static final String SEPARATOR = ",";
+	private static final String SEPARATOR = ";";
+	private static final String DECIMAL_SEPARATOR_EN = ".";
+	private static final String DECIMAL_SEPARATOR_FR = ".";
 
 	/**
 	 * It's a row to build a CSV.
@@ -19,7 +21,7 @@ public class RowCSV {
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		for (String data : columns) {
-			builder.append(data);
+			builder.append(data.replace(DECIMAL_SEPARATOR_EN, DECIMAL_SEPARATOR_FR));
 			builder.append(SEPARATOR);
 		}
 		return builder.replace(builder.length() - 1, builder.length(), "").toString();
