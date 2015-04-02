1786512f06de54e655f085e0da83e7436099f618
Guillaume Ruscassie <guillaume.ruscassie@icloud.com>
2015-04-02 20:43:27 +0200

    Add the search if the terms are instance

--- moviesearch/src/main/java/fr/univtls2/web/moviesearch/services/query/SparqlQueryExecutor.java
+++ moviesearch/src/main/java/fr/univtls2/web/moviesearch/services/query/SparqlQueryExecutor.java
@@ -2,6 +2,7 @@ package fr.univtls2.web.moviesearch.services.query;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -19,37 +20,50 @@ import fr.univtls2.web.sparql.SparqlRequest;
 
 public class SparqlQueryExecutor implements QueryExecutor {
 
-	@Inject private TermDao dao;
-	@Inject	private Extractor extractor;
-	@Inject	private Normalizer normalizer;
-	@Inject	private Set<SearchWeigher> weightRules;
+	@Inject
+	private TermDao dao;
+	@Inject
+	private Extractor extractor;
+	@Inject
+	private Normalizer normalizer;
+	@Inject
+	private Set<SearchWeigher> weightRules;
 
 	@Override
-	public List<SourceDoc> execute(String query) {
+	public List<SourceDoc> execute(String pQuery) {
 		SparqlClient sparqlClient = new SparqlClient("localhost:8080/space");
 		SparqlRequest sparqlRequest = new SparqlRequest();
 
-		List<SourceDoc> results = new ArrayList<SourceDoc>();
-		List<Term> termsUser = extractor.extract(query);
+		List<SourceDoc> results = null;
+		List<Term> termsUser = extractor.extract(pQuery);
 		List<Term> termsInstance = new ArrayList<Term>();
 		List<Term> termsOther = new ArrayList<Term>();
-		
-		//We test the term to know if a instance of the term exist
-		//if the term exist then we put it in other list to use later in a select
-		//TODO bug when we add in termsInstance the term 
+
+		// We test the term to know if a instance of the term exist
+		// if the term exist then we put it in other list to use later in a select
+		// TODO bug when we add in termsInstance the term
 		for (Term term : termsUser) {
 			if (sparqlClient.ask(sparqlRequest.generatorAsk(term))) {
 				termsInstance.add(term);
-			}else{
+			} else {
 				termsOther.add(term);
 			}
 		}
-		
-		if(!termsInstance.isEmpty() && termsOther.isEmpty()){
-			Collection<Map<String, String>> result = sparqlClient.select(sparqlRequest.generatorSelect(termsInstance));
-			//TODO Just to see replace by a the old treatment did on the user term.
-			for(Map<String, String> map : result){
-				for(String v : map.values()){
+
+		if (!termsInstance.isEmpty() && termsOther.isEmpty()) {
+			Collection<Map<String, String>> resultSparql = sparqlClient.select(sparqlRequest.generatorSelect(termsInstance));
+			// TODO Just to see replace by a the old treatment did on the user term.
+			StringBuilder newQuery = new StringBuilder();
+			for (Map<String, String> map : resultSparql) {
+				for (String v : map.values()) {
+					newQuery.append(v).append(" ");
+				}
+			}
+			results = search(newQuery.toString());
+		} else {
+			Collection<Map<String, String>> resultLinks = sparqlClient.select(sparqlRequest.generatorFilterLink(termsOther));
+			for (Map<String, String> map : resultLinks) {
+				for (String v : map.values()) {
 					System.out.println(v);
 				}
 			}
@@ -63,13 +77,52 @@ public class SparqlQueryExecutor implements QueryExecutor {
 		// naissance -> lien
 		// omar sy -> instance
 		// TODO Guillaume Ruscassie
-		return null;
+		return results;
 	}
 
-	private void searchInstances(List<Term> pTermsToFind) {
-		StringBuilder query = new StringBuilder();
-		// String query =
-		// "SELECT distinct ?p WHERE {{inst:Intouchables ?t1 ?o1. ?p ?t1 inst:Personne;} UNION {inst:Personne ?t1 ?o1. ?p ?t1 inst:Intouchables;}}";
+	private List<SourceDoc> search(String pQuery) {
+		List<Term> termsToFind = extractor.extract(pQuery);
+		termsToFind = normalizer.normalize(termsToFind);
+		List<SourceDoc> results = new ArrayList<SourceDoc>();
 
+		Collection<Term> termsFound = dao.findByWords(termsToFind);
+		for (Term termFound : termsFound) {
+			if (results.isEmpty()) {
+				results.addAll(termFound.getDocuments());
+				for (SourceDoc result : results) {
+					result.setTagsList(new ArrayList<List<String>>());
+					result.setPositionsList(new ArrayList<List<Integer>>());
+					result.getTagsList().add(result.getTags());
+					result.getPositionsList().add(result.getPositions());
+				}
+			} else {
+				results.retainAll(termFound.getDocuments());
+				for (SourceDoc result : results) {
+					int docIndex = termFound.getDocuments().indexOf(result);
+					SourceDoc docFound = termFound.getDocuments().get(docIndex);
+					mergeDoc(docFound, result);
+				}
+			}
+		}
+
+		for (SourceDoc result : results) {
+			for (SearchWeigher weigher : weightRules) {
+				weigher.weight(result);
+			}
+		}
+
+		Collections.sort(results);
+		return results;
+	}
+	
+	/**
+	 * Merge two documents.
+	 * @param termFound : the term with its list of document.
+	 * @param result : the document to merge.
+	 */
+	private void mergeDoc(SourceDoc docFound, SourceDoc result) {
+		result.setWeight(result.getWeight() + docFound.getWeight());
+		result.getTagsList().add(docFound.getTags());
+		result.getPositionsList().add(docFound.getPositions());
 	}
 }
